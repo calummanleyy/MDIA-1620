eb1f56d417b245bebe4fb3b8310fb93aefa08585
calummanleyy <cmanley6@my.bcit.ca>
2024-09-19 15:50:36 -0700

    add horse or something yes

---
+++ stables.js
@@ -0,0 +1,11 @@
+console.log("horses are BAD")
+let horseName = "Coolfella"; //this is a string
+horseName = "gamer"; //this name takes over because of placement
+let horseAge = 5; //numbers dont need quotes but always use double quotes otherwise
+console.log(horseName);
+console.log(horseAge);
+let isHorseInStable = true; //boolean!!
+console.log(isHorseInStable);
+isHorseInStable = true;
+horseName = "bangie bang the horsey";
+console.log(horseName);
