861da829cabde840ea5e8da72a52100c4d6d1f24
calummanleyy <cmanley6@my.bcit.ca>
2024-09-26 14:37:23 -0700

    add operations to my stable

--- stables.js
+++ stables.js
@@ -2,10 +2,23 @@ console.log("horses are BAD")
 let horseName = "Coolfella"; //this is a string
 horseName = "gamer"; //this name takes over because of placement
 let horseAge = 5; //numbers dont need quotes but always use double quotes otherwise
-console.log(horseName);
+console.log(horseName); // logs earliest horse name ^^
 console.log(horseAge);
 let isHorseInStable = true; //boolean!!
 console.log(isHorseInStable);
 isHorseInStable = true;
-horseName = "bangie bang the horsey";
+horseName = "bangie bang the horsey"; //this horse name overwrites others but is its own
 console.log(horseName);
+const STABLE_MONTHLY_FEE = 666;
+
+console.log(horseName + " is the name of my horse ");
+let horseIntroduction = " is the name of my horse! "
+console.log(horseName + horseIntroduction)
+
+//let rentIntroduction =" it costs " + STABLE_MONTHLY_FEE + " to board " + horseName
+let rentIntroduction = `It costs ${STABLE_MONTHLY_FEE} to board ${horseName}`
+console.log(rentIntroduction)
+
+//let stableIntroduction =" Welcome to " + horseName + "'s stable :D "
+let stableIntroduction = `Welcome to ${horseName}'s stable :D`
+console.log(stableIntroduction)
