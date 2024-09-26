2eb78082e88358ae0cfd36179bfe2af25b0be620
calummanleyy <cmanley6@my.bcit.ca>
2024-09-26 15:50:12 -0700

    nickname dicount lab 2

--- stables.js
+++ stables.js
@@ -9,7 +9,7 @@ console.log(isHorseInStable);
 isHorseInStable = true;
 horseName = "bangie bang the horsey"; //this horse name overwrites others but is its own
 console.log(horseName);
-const STABLE_MONTHLY_FEE = 666;
+const STABLE_MONTHLY_FEE = 10;
 
 console.log(horseName + " is the name of my horse ");
 let horseIntroduction = " is the name of my horse! "
@@ -21,4 +21,16 @@ console.log(rentIntroduction)
 
 //let stableIntroduction =" Welcome to " + horseName + "'s stable :D "
 let stableIntroduction = `Welcome to ${horseName}'s stable :D`
-//console.log(stableIntroduction)
+console.log(stableIntroduction)
+
+//LAB 2 VVVVV
+
+let horseNickname = "bacon"
+let horseNicknameIntro = horseName + " is the name of my horse! and " + horseNickname + " is his nickname. " 
+console.log(horseNicknameIntro)
+
+
+let discount = (STABLE_MONTHLY_FEE *3 - 3)
+let discountMessage = `Save 10 percent off THREE whole months which adds up to a total of ${discount} smackaroons.` 
+console.log(discountMessage)
+
