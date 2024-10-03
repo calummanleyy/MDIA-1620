e8025b7be3755d2db64df4c340ba225a4e30eafd
calummanleyy <cmanley6@my.bcit.ca>
2024-10-02 20:05:58 -0700

    horse discount

--- stables.js
+++ stables.js
@@ -1,28 +1,23 @@
-console.log("horses are BAD")
+//console.log("horses are BAD")
 let horseName = "Coolfella"; //this is a string
 horseName = "gamer"; //this name takes over because of placement
 let horseAge = 5; //numbers dont need quotes but always use double quotes otherwise
-console.log(horseName);
-console.log(horseAge);
 let isHorseInStable = true; //boolean!!
-console.log(isHorseInStable);
 isHorseInStable = true;
 
 horseName = "bangie bang the horsey"; //this horse name overwrites others but is its own
-console.log(horseName);
 const STABLE_MONTHLY_FEE = 10;
 
-console.log(horseName + " is the name of my horse ");
 let horseIntroduction = " is the name of my horse! "
-console.log(horseName + horseIntroduction)
+//console.log(horseName + horseIntroduction)
 
 //let rentIntroduction =" it costs " + STABLE_MONTHLY_FEE + " to board " + horseName
 let rentIntroduction = `It costs ${STABLE_MONTHLY_FEE} to board ${horseName}`
-console.log(rentIntroduction)
+//console.log(rentIntroduction)
 
 //let stableIntroduction =" Welcome to " + horseName + "'s stable :D "
 let stableIntroduction = `Welcome to ${horseName}'s stable :D`
-console.log(stableIntroduction)
+//console.log(stableIntroduction)
 
 //LAB 2 VVVVV
 
@@ -31,6 +26,9 @@ let horseNicknameIntro = horseName + " is the name of my horse! and " + horseNic
 console.log(horseNicknameIntro)
 
 
-let discount = (STABLE_MONTHLY_FEE *3 - 3)
-let discountMessage = `Save 10 percent off THREE whole months which adds up to a total of ${discount} smackaroons.` 
-console.log(discountMessage)
+const totalForThreeMonths = STABLE_MONTHLY_FEE * 3; // Total for three months
+const discount = totalForThreeMonths * 0.1; // 10% discount
+const totalAfterDiscount = totalForThreeMonths - discount; // Total after discount
+
+let totalMessage = `The total after applying the 10 percent discount for THREE whole months is ${totalAfterDiscount} smackaroons.`;
+console.log(totalMessage);
